3cc4031073d8711009c1842c22d03df54ff76e5b
Bence Petheo <bence.petheo@ge.com>
2018-11-29 00:49:25 +0100

    implemented quick action mode

--- parkalot.py
+++ parkalot.py
@@ -45,110 +45,6 @@ def login():
         driver.quit()
 
 
-def get_green_days(ignored_days):
-    # Get the days that where there are available spots for reservation
-    days = []
-    for day in driver.find_elements_by_xpath('//div[@class="box-color m-t-md sharp-shadow dark r m-l m-r"]'):
-        # The status is either "<n> spots available" or "No spots available" or "<building>  <spot>"
-        #  so the first word is only a valid integer when there are spots available
-        #  (that are not already booked by you) so we use this as a check.
-        try:
-            free_spots = int(day.find_element_by_xpath('.//span[contains(@class,"_600")]').text.split(' ')[0])
-            day_name = day.find_element_by_xpath('.//span[@class="pull-left _300"]/span').text.lower()
-
-            # Don't send alert for ignored days if any is given
-            if not (ignored_days and day_name in ignored_days):
-                days.append({
-                    'name': day_name,
-                    'free_spots': free_spots,
-                    'btn_more': day.find_element_by_xpath('.//button[contains(text(), "More")]'),
-                    'btn_reserve': day.find_element_by_xpath('.//button[contains(text(), "Reserve")]'),
-                })
-                debug_print('{} spot(s) found for {}!'.format(free_spots, day_name), 1)
-        except (ValueError, NoSuchElementException, StaleElementReferenceException):
-            pass
-    return days
-
-
-def get_free_spots():
-    # Cycle through the day boxes and collect those that have the 'Available' status message
-    free_spots = []
-    for spot in driver.find_elements_by_xpath('//div[@class="box"]'):
-        try:
-            spot_number = ' '.join(spot.find_element_by_xpath('.//h3[@class="text-ellipsis"]').text.split())
-            if 'Available' in spot.get_attribute('innerHTML'):
-                debug_print('{}: FREE'.format(spot_number), 2)
-                free_spots.append({
-                    'number': spot_number,
-                    'btn_reserve': spot.find_element_by_xpath('.//button[contains(text(), "Reserve")]'),
-                })
-            else:
-                debug_print('{}: OCCUPIED'.format(spot_number), 2)
-        except StaleElementReferenceException:
-            pass
-
-    # Sort the results by the 'number' attribute before returning
-    return sorted(free_spots, key=lambda k: k['number'])
-
-
-def get_spot_by_number(spot_number, available_spots):
-    # Check if spot number is in the available_spots list
-    # returns the spot object or 'None' if it's not in the list
-    for spot in available_spots:
-        if spot['number'] == spot_number:
-            return spot
-    return None
-
-
-def do_reservation(btn_reserve):
-    # Clicks the provided 'Reserve' button. Waits for confirmation
-    time.sleep(1)
-    btn_reserve.click()
-    debug_print('Waiting for reservation confirmation...', 2)
-    try:
-        driver.find_element_by_xpath('//div[contains(@class, "snackbar") and text() = "Successfully reserved"]')
-        debug_print('Reservation confirmed', 1)
-    except NoSuchElementException:
-        debug_print('Captcha, refreshing', 1)
-        driver.refresh()
-
-
-def reserve_spot(day):
-    # Reverse a spot on a given day
-    # In case multiple spots are available, it tries to reserve one from the preferred list
-    debug_print('Reserving spot...', 1)
-
-    if day['free_spots'] <= 0:
-        # Do nothing if there aren't any free spots
-        debug_print('No free spots, skipping', 1)
-        return
-    elif day['free_spots'] == 1:
-        # If there's only one free spot simply reserve it
-        debug_print('Only one spot available, reserving that one', 1)
-        do_reservation(day['btn_reserve'])
-        return
-    else:
-        # If there are multiple free spots, try to reserve the preferred ones first
-        debug_print('Multiple spots are available, trying to reserve a preferred one', 1)
-        original_url = driver.current_url
-        day['btn_more'].click()
-        free_spots = get_free_spots()
-        for ps in preferred_spots:
-            spot = get_spot_by_number(ps, free_spots)
-            if spot:
-                debug_print('Preferred spot {} is free, reserving'.format(ps), 1)
-                do_reservation(spot['btn_reserve'])
-                driver.get(original_url)
-                return
-            else:
-                debug_print('Preferred spot {} is not free, skipping'.format(ps), 1)
-
-        # If none of the preferred spaces are available simply reserve the first one available
-        if len(free_spots) > 0:
-            do_reservation(free_spots[0]['btn_reserve'])
-        driver.get(original_url)
-
-
 def refresh_if_needed():
     # Refresh page periodically (currently every hour)
     now = time.localtime()
@@ -207,12 +103,15 @@ if __name__ == '__main__':
                 login()
 
             # Here's where the magic happens
-            green_days = get_green_days(args.ignore)
-            for green_day in green_days:
-                reserve_spot(green_day)
+            try:
+                debug_print('Looking for a reserve button', 2)
+                btn = driver.find_element_by_xpath('.//button[contains(text(), "Reserve")]')
+                debug_print('Found a spot, reserving', 1)
+                btn.click()
+            except (NoSuchElementException, StaleElementReferenceException):
+                debug_print('Not found, trying again', 2)
+                pass
 
-            # Sleep the main loop for a second to avoid being a resource hog
-            time.sleep(1)
     except KeyboardInterrupt:
         pass
     finally:
